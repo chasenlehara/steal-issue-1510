e5b14a43b1530faa29455dbdeede63999ae66562
Chasen Le Hara <git@chasenlehara.com>
2019-10-21 13:11:14 +0100

    Add example code

---
+++ index.js
@@ -0,0 +1,3 @@
+// import layout from "simple-keyboard-layouts/build/layouts/french";
+import layout from "./module-does-not-work";
+console.info("Imported layout:", layout);
---
+++ module-does-not-work.js
@@ -0,0 +1,1 @@
+const french={default:"//"};export default french;
